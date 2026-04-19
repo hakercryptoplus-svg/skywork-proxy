05bd767f364fcc25be0dbd9f3abbf5bf28e44a9c
Ahmad <ahmad@skywork-proxy.bot>
2026-04-19 07:28:33 +0300

    Add background collector + telegram control + auto-push

--- state.js
+++ state.js
@@ -1,17 +1,41 @@
 const fs = require('fs');
 const path = require('path');
 
+const FILE = path.join(__dirname, 'tokens.json');
+
 let tokens = [];
+let tokenSet = new Set();
 
 try {
-  tokens = JSON.parse(fs.readFileSync(path.join(__dirname, 'tokens.json'), 'utf8'));
+  tokens = JSON.parse(fs.readFileSync(FILE, 'utf8'));
+  tokenSet = new Set(tokens);
 } catch {
   tokens = [];
+  tokenSet = new Set();
 }
 
 module.exports = {
   getTokens: () => tokens,
-  addToken: (t) => { tokens.push(t); },
-  removeToken: (i) => { tokens.splice(i, 1); },
-  setTokens: (t) => { tokens = [...t]; }
+  hasToken: (t) => tokenSet.has(t),
+  addToken: (t) => {
+    if (tokenSet.has(t)) return false;
+    tokens.push(t);
+    tokenSet.add(t);
+    return true;
+  },
+  removeToken: (i) => {
+    const t = tokens[i];
+    if (t) tokenSet.delete(t);
+    tokens.splice(i, 1);
+  },
+  setTokens: (t) => {
+    tokens = [...t];
+    tokenSet = new Set(tokens);
+  },
+  persist: () => {
+    const tmp = FILE + '.tmp';
+    fs.writeFileSync(tmp, JSON.stringify(tokens));
+    fs.renameSync(tmp, FILE);
+  },
+  serialize: () => JSON.stringify(tokens)
 };
